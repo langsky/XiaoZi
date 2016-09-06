facdb10f0a8d0d5845046969c2c33a4af5567dab
langsky <langsky001@hotmail.com>
2016-09-06 21:22:12 +0800

    update

---
+++ app/src/main/java/com/ican/langsky/xiaozi/model/TaxIncome.java
@@ -0,0 +1,23 @@
+package com.ican.langsky.xiaozi.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by hgl on 16-9-6.
+ */
+public class TaxIncome extends RealmObject{
+    @PrimaryKey
+    private String time_id;
+    private float incomeTax;
+
+    public TaxIncome setTimeData(String timeData) {
+        this.time_id = timeData;
+        return this;
+    }
+
+    public TaxIncome setTax(float tax) {
+        incomeTax = tax;
+        return this;
+    }
+}
